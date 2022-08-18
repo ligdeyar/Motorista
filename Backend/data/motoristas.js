6c1289417eca8b5d554774724d3be06fcfe91f9b
ligdeyar <ligdeyar.zep98@gmail.com>
2022-08-17 20:51:02 -0600

    Add motorista VHDL

---
+++ Backend/data/motoristas.js
@@ -0,0 +1,56 @@
+[
+    {
+        "codigoMotorista": 1,
+        "nombreMotorista": "juan",
+        "contrasena": "juan1",
+        "correo": " juan@instafood.hn",
+        "ordenesTomada": [
+            1,
+            2
+        ],
+        "ordenesEntregadas": [
+            1,
+            3
+        ]
+    },
+    {
+        "codigoMotorista": 2,
+        "nombreMotorista": "Yare",
+        "contrasena": "yare2",
+        "correo": " yare@instafood.hn",
+        "ordenesTomada": [
+            3,
+            2
+        ],
+        "ordenesEntregadas": [
+            1
+        ]
+    },
+    {
+        "codigoMotorista": 3,
+        "nombreMotorista": "Ana",
+        "contrasena": "Ana1",
+        "correo": " ana@instafood.hn",
+        "ordenesTomada": [
+            1,
+            2
+        ],
+        "ordenesEntregadas": [
+            1,
+            3
+        ]
+    },
+    {
+        "codigoMotorista": 4,
+        "nombreMotorista": "Julia",
+        "contrasena": "julia1",
+        "correo": " julia@instafood.hn",
+        "ordenesTomada": [
+            1
+        ],
+        "ordenesEntregadas": [
+            1,
+            2
+        ]
+    }
+]
